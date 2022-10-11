aa486d43448698b26bd6bdad62f1164091e1408c
dfunani <dfunani@live.co.za>
2022-10-12 00:36:57 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Delali Funani";
+            Console.WriteLine($"Hello, please enter\n name: ");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!!!");
+            Console.ReadKey(true);
+        }
+    }
+}
